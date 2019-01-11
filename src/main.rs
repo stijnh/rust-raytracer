66967ffc4e7caaafe775a1d28f1b77dc7e9ef2f3
stijnh <mail@stijnh.nl>
2019-01-11 17:32:31 +0100

    Added main file for testing.

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,45 @@
 mod common;
 mod geom;
 mod integrator;
+mod light;
+mod loader;
 mod material;
 mod math;
 mod render;
 mod scene;
 mod texture;
-mod light;
 
 use crate::math::*;
+use crate::geom::*;
+use crate::texture::*;
 use std::sync::Arc;
 
 pub fn main() {
-    let mut list = vec![];
-    for j in -10..=20 {
-        for i in -10..=20 {
-            list.push(geom::Sphere::new(vec3d(j as f32, i as f32, 0.0), 0.3));
-        }
-    }
+    let mut list: Vec<Box<dyn Geometry>> = vec![];
+    let mesh = loader::load_ply_as_mesh("bunny_big.ply").unwrap();
+    let mesh = Transform::new(mesh)
+        .scale(20.0)
+        .rotate_x(90f32.to_radians());
 
-    let obj_arc = Arc::new(geom::AABBTree::new(list, 1.0));
+    let bb = mesh.bounding_box();
+    println!("{:?}", bb);
+    let cube = Cuboid::new(
+        vec3d(-5.0, -5.0, bb.min[2]),
+        vec3d(5.0, 5.0, bb.min[2] - 1.0),
+    );
+
+
+    list.push(Box::new(mesh));
+    list.push(Box::new(cube));
+
+    let obj_arc = Arc::new(geom::GeometryList::from_vec(list));
     let mut integrator = integrator::WhittedIntegrator::new();
-    integrator.antialiasing = 10;
+    integrator.antialiasing = 4;
+    integrator.shadow_rays = 20;
 
-    let pos = Vec3D::new(-5.0, -6.0, 3.0);
+    let pos = Vec3D::new(-2.5, -3.0, 3.0);
     let up = -Vec3D::z_axis();
-    let focus = Vec3D::new(-4.0, -4.0, 0.0);
+    let focus = Vec3D::new(-0.0, -0.0, 0.0);
     let cam = scene::Camera::new(800, 600)
         .position(pos)
         .look_at(focus, up);
@@ -36,9 +50,27 @@ pub fn main() {
     let skybox = texture::Image::open("skybox.jpg").unwrap();
 
     let lights: Vec<Box<dyn light::Light>> = vec![
-        Box::new(light::AmbientLight::new(Vec3D::one(), 0.1)),
+        //Box::new(light::AmbientLight::new(Vec3D::one(), 0.0)),
         //Box::new(light::DirectionLight::new(vec3d(-1.0, -1.0, -1.0), 0.0, Vec3D::one(), 1.0)),
-        Box::new(light::PointLight::new(Vec3D::new(-4.0, -4.0, 3.0), 0.0, vec3d(1.0, 1.0, 1.0), 10.0)),
+        /*
+        Box::new(light::PointLight::new(
+            pos, //Vec3D::new(-0.0, 0.0, 3.0),
+            0.0,
+            vec3d(1.0, 1.0, 1.0),
+            10.0,
+        )),
+        */
+        Box::new(
+            light::DirectionLight::new(
+                vec3d(-2.5, -3.0, -3.0),
+                0.0,
+                COLOR_WHITE,
+                0.5)
+        ),
+
+        Box::new(
+            light::AmbientOcclusion::new(0.3, Vec3D::one(), 0.5)
+        ),
         //scene::Light::new_directional(Vec3D::fill(0.45), vec3d(1.0, 1.0, 1.0)),
         //scene::Light::new_directional(Vec3D::fill(0.45), vec3d(-1.0, 1.0, 1.0)),
     ];
@@ -49,6 +81,16 @@ pub fn main() {
         skybox: Arc::new(skybox),
         lights,
     };
+
+
+    for i in 1..10 {
+        let mut fast_integrator = integrator.clone();
+        fast_integrator.shadow_rays = i * 10;
+        fast_integrator.antialiasing = 1;
+        println!("{:?}", fast_integrator);
+        let img = render::parallel_render_image(&scene, &fast_integrator);
+        img.save(&format!("test_{}.png", i)).unwrap();
+    }
 
     let img = render::parallel_render_image(&scene, &integrator);
     img.save("test.png").unwrap();
