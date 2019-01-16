51d25429cb9b567aa985c4ea36c3bc9685a73349
stijnh <mail@stijnh.nl>
2019-01-16 17:28:32 +0100

    New main file with some tests.

--- src/main.rs
+++ src/main.rs
@@ -32,19 +32,23 @@ pub fn main() {
         vec3d(5.0, 5.0, bb.min[2] - 1.0),
     );
 
-    list.push(Object::with_material(
-            mesh.clone(), 
-            Glass));
+    let c = (bb.min + bb.max) / 2.0;
+    let r = 1.0;
+    //let mesh = Sphere::new(c, r);
 
     list.push(Object::with_material(
-            Translate::new(mesh.clone(), vec3d(0.0, -2.5, 0.0)),
+            mesh.clone(), 
+            Transparent(1.0 / 0.6)));
+
+    list.push(Object::with_material(
+            Translate::new(mesh.clone()).translate_y(-2.5),
             Metal));
 
     list.push(Object::with_material(
-            Translate::new(mesh.clone(), vec3d(0.0, 2.5, 0.0)),
+            Translate::new(mesh.clone()).translate_y(2.5),
             Lambartian(COLOR_WHITE)));
 
-    list.push(Object::with_material(cube, Glossy(1e3)));
+    list.push(Object::with_material(cube, Glossy(1e3, 0.1, COLOR_WHITE)));
 
     let obj_arc = Arc::new(geom::GeometryList::from_vec(list));
     let mut integrator = integrator::WhittedIntegrator::new();
@@ -53,7 +57,7 @@ pub fn main() {
 
     let pos = Vec3D::new(-6.0, -6.0, 3.0);
     let up = -Vec3D::z_axis();
-    let focus = Vec3D::new(-0.0, -0.0, 0.0);
+    let focus = c + Vec3D::new(0.0, 0.0, 0.1); //Vec3D::new(-0.0, -0.0, 0.0);
     let cam = scene::Camera::new(1080, 960)
         .position(pos)
         .look_at(focus, up);
@@ -64,11 +68,11 @@ pub fn main() {
         Box::new(AmbientLight::new(Vec3D::one(), 0.1)),
         Box::new(DirectionLight::new(
             vec3d(-2.5, -3.0, -3.0),
-            0.0,
+            0.01,
             COLOR_WHITE,
-            0.4,
+            0.6,
         )),
-        Box::new(AmbientOcclusion::new(1e12, Vec3D::one(), 0.5)),
+        Box::new(AmbientOcclusion::new(1e12, Vec3D::one(), 0.3)),
     ];
 
     let scene = scene::Scene {
@@ -78,10 +82,12 @@ pub fn main() {
         lights,
     };
 
-    for i in 5..100 {
+    for i in 1..100 {
         let mut fast_integrator = integrator.clone();
-        fast_integrator.shadow_rays = i;
+        fast_integrator.shadow_rays = 1;
+        fast_integrator.scatter_rays = 1;
         fast_integrator.antialiasing = i;
+        fast_integrator.max_depth = 1;
         println!("{:?}", fast_integrator);
         let img = render::parallel_render_image(&scene, &fast_integrator);
         img.save(&format!("test_{}.png", i)).unwrap();
